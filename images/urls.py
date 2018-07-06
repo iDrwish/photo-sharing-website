865aa349a8863ffff82b65af4100bd98b4b9382e
mohamed-darwish <mu.darwish23@gmail.com>
2018-07-06 17:54:51 +0200

    Created a common decorator to validate AJAX requests overt HTTP

--- images/urls.py
+++ images/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^create/$', views.image_create, name='create'),
     url('^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='details'),
-    url('^like/$', views.image_like, name='like')
+    url('^like/$', views.image_like, name='like'),
+url(r'^$', views.image_list, name='list')
 ]
