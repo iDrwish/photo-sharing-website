11acd52315b30f87604182c9f64f8df53cc3c76b
mohamed-darwish <mu.darwish23@gmail.com>
2018-07-29 19:15:17 +0200

    Added a receiver function in signals.py to update total_likes per change in user_like

--- images/signals.py
+++ images/signals.py
@@ -6,5 +6,7 @@ from .models import Image
 @receiver(m2m_changed, sender=Image.user_like.through)
 def user_like_changed(sender, instance, **kwargs):
     '''A reciever function to update the number of total likes per image after each action'''
+    '''This function works syncronous, might consider caching, asynronous calls, or even cron jobs
+    to populate it'''
     instance.total_likes = instance.user_like.count()
     instance.save()
