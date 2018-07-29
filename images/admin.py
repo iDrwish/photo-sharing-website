f7a1114594934023128383214bcbf77361add768
mohamed-darwish <mu.darwish23@gmail.com>
2018-07-29 21:07:13 +0200

    working on fixing signals bug that won't update total_likes

--- images/admin.py
+++ images/admin.py
@@ -6,4 +6,8 @@ class ImageAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'image', 'created', 'user', 'total_likes']
     list_filter = ['created']
 
+
+    def get_products(self, obj):
+        return "\n".join([p.user_like for p in obj.objects.all()])
+
 admin.site.register(Image, ImageAdmin)
